51a4ef0033a76dc9841f6a1a2982119d87e9574f
Fatima Amin <fatima.amin4official@gmail.com>
2020-09-15 13:54:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 
 // Write your code here
 
-let multiplye = () => {
+
   let multiply;
   let num1 = 4 ;
   console.log(Number.isInteger(num1));
   let  num2 =15.5;
   multiply =  num1 * num2 ;
-}
-multiplye();
+
 
 
 function random  (){

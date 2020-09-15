e942238f16d5e16e205b5c060f83915475966b0d
Fatima Amin <fatima.amin4official@gmail.com>
2020-09-15 13:44:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
 // Write your code here
 
-multiply =(num1, num2) => {
+multiply =() => {
   let multiply , num1 = 4 , num2 =15.5;
   multiply =  num1 * num2 ;
 }
-multiply(4 , 15.5);
+multiply(multiply);
 
 
 random = () =>{
